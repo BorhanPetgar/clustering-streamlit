cc9861c00054bb3483da8989d29b5bfc9226ed8b
BorhanPetgar <borhanpetgar40@gmail.com>
2024-05-03 20:57:09 +0330

    other algorithms and dataset added

--- clf_app.py
+++ clf_app.py
@@ -1,81 +1,108 @@
 import streamlit as st
 from sklearn.datasets import make_blobs, make_circles, make_moons
 from numpy import unique
-from sklearn.cluster import KMeans
-from sklearn.cluster import AgglomerativeClustering, DBSCAN
+from sklearn.cluster import KMeans, AgglomerativeClustering, DBSCAN, SpectralClustering, AffinityPropagation, MeanShift
 import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.mixture import GaussianMixture
 
-
-def load_dataset(dataset_name):
-    if dataset_name == 'Aggregation':
-        X, y = make_blobs(n_samples=1000, centers=4, random_state=42)
-    elif dataset_name == 'Blobs':
-        X, y = make_blobs(n_samples=1000, centers=5, random_state=42)
-    elif dataset_name == 'Circles':
-        X, y = make_circles(n_samples=1000, noise=0.05, random_state=42)
-    elif dataset_name == 'Moons':
-        X, y = make_moons(n_samples=1000, noise=0.05, random_state=42)
-    return X, y
-
-def load_algorithm(clustering_algorithm):
-    if clustering_algorithm == 'K-Means':
-        n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2)
-        algorithm = KMeans(n_clusters=n_clusters)
-    elif clustering_algorithm == 'Agglomerative':
-        n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2)
-        linkage = st.sidebar.selectbox('Linkage', ('ward', 'complete', 'average'))
-        algorithm = AgglomerativeClustering(n_clusters=n_clusters, linkage=linkage)
-    elif clustering_algorithm == 'DBSCAN':
-        eps = st.sidebar.slider('Epsilon', min_value=0.1, max_value=1.0, value=0.5, step=0.05)
-        min_samples = st.sidebar.slider('Minimum Samples', min_value=1, max_value=10, value=5)
-        algorithm = DBSCAN(eps=eps, min_samples=min_samples)
+class StreamlitApp:
+    def __init__(self) -> None:
+        self.dataset_name = st.sidebar.selectbox('Choose your desired dataset please: ',
+                                            ('Aggregation', 'Blobs', 'Circles', 'Moons', 'Noisy Circles', 'Synthetic Moons', 'Synthetic Circles', 'Synthetic Blobs'))
+        self.clustering_algorithm = st.sidebar.selectbox('Choose Clustering Algorithm: ',
+                                                    ('K-Means', 'Agglomerative', 'DBSCAN', 'Spectral Clustering', 'Affinity Propagation', 'Mean Shift', 'Gaussian Mixture Models'))
+        if self.clustering_algorithm == 'K-Means':
+            self.n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2, key='kmeans')
+        elif self.clustering_algorithm == 'Agglomerative':
+            self.n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2, key='agg')
+            self.linkage = st.sidebar.selectbox('Linkage', ('ward', 'complete', 'average'))
+        elif self.clustering_algorithm == 'DBSCAN':
+            self.eps = st.sidebar.slider('Epsilon', min_value=0.1, max_value=1.0, value=0.5, step=0.05, key='dbscan')
+            self.min_samples = st.sidebar.slider('Minimum Samples', min_value=1, max_value=10, value=5, key='dbscan2')
+        elif self.clustering_algorithm == 'Spectral Clustering':
+            self.n_clusters = st.sidebar.slider('Number of Clusters', min_value=2, max_value=10, value=2, key='spectral')
+        elif self.clustering_algorithm == 'Affinity Propagation':
+            self.damping = st.sidebar.slider('Damping', min_value=0.5, max_value=1.0, value=0.5, step=0.1, key='affinity')
+        elif self.clustering_algorithm == 'Mean Shift':
+            self.bandwidth = st.sidebar.slider('Bandwidth', min_value=0.1, max_value=1.0, value=0.5, step=0.1, key='meanshift')
+        elif self.clustering_algorithm == 'Gaussian Mixture Models':
+            self.n_components = st.sidebar.slider('Number of Components', min_value=2, max_value=10, value=2, key='gmm')
+    
+    def run(self):
+        self.__add_title()
+        self.__load_dataset(self.dataset_name)
+        self.__load_algorithm(self.clustering_algorithm)
+        self.__apply_clustering()
+        self.__dispaly()
+    
+    def __add_title(self):
+        st.title(':blue[C]:red[L]:blue[U]:red[S]:blue[T]:red[E]:blue[R]:red[I]:blue[N]:red[G] :sunglasses:')
+        st.write("""
+                # Explore **CLUSTERING** algorithms on different dataset
+                """)
+        st.write(f'Selected dataset: {self.dataset_name}')
         
-    return algorithm
+    def __load_dataset(self, dataset_name):
+        if dataset_name == 'Aggregation':
+            self.X, self.y = make_blobs(n_samples=1000, centers=4, random_state=42)
+        elif dataset_name == 'Blobs':
+            self.X, self.y = make_blobs(n_samples=1000, centers=5, random_state=42)
+        elif dataset_name == 'Circles':
+            self.X, self.y = make_circles(n_samples=1000, noise=0.05, random_state=42)
+        elif dataset_name == 'Moons':
+            self.X, self.y = make_moons(n_samples=1000, noise=0.05, random_state=42)
+        elif dataset_name == 'Noisy Circles':
+            self.X, self.y = make_circles(n_samples=1000, noise=0.03, factor=0.5, random_state=42)
+        elif dataset_name == 'Synthetic Moons':
+            self.X, self.y = make_moons(n_samples=1000, noise=0.03, random_state=42)
+        elif dataset_name == 'Synthetic Circles':
+            self.X, self.y = make_circles(n_samples=1000, noise=0.03, random_state=42)
+        elif dataset_name == 'Synthetic Blobs':
+            self.X, self.y = make_blobs(n_samples=1000, centers=5, cluster_std=[1.0, 2.5, 0.5, 3.0, 1.5], random_state=42)
 
-st.title(':blue[C]:red[L]:blue[U]:red[S]:blue[T]:red[E]:blue[R]:red[I]:blue[N]:red[G] :sunglasses:')
+    def __load_algorithm(self, clustering_algorithm):
+        if clustering_algorithm == 'K-Means':
+            self.algorithm = KMeans(n_clusters=self.n_clusters)
+        elif clustering_algorithm == 'Agglomerative':
+            self.algorithm = AgglomerativeClustering(n_clusters=self.n_clusters, linkage=self.linkage)
+        elif clustering_algorithm == 'DBSCAN':
+            self.algorithm = DBSCAN(eps=self.eps, min_samples=self.min_samples)
+        elif clustering_algorithm == 'Spectral Clustering':
+            self.algorithm = SpectralClustering(n_clusters=self.n_clusters)
+        elif clustering_algorithm == 'Affinity Propagation':
+            self.algorithm = AffinityPropagation(damping=self.damping)
+        elif clustering_algorithm == 'Mean Shift':
+            self.algorithm = MeanShift(bandwidth=self.bandwidth)
+        elif clustering_algorithm == 'Gaussian Mixture Models':
+            self.algorithm = GaussianMixture(n_components=self.n_components)
 
-st.write("""
-         # Explore **CLUSTERING** algorithms on different dataset
-         """)
+        return self.algorithm
+    
+    def __apply_clustering(self):
+        self.__load_dataset(dataset_name=self.dataset_name)
+        self.__load_algorithm(clustering_algorithm=self.clustering_algorithm)
+        self.cluster_labels = self.algorithm.fit_predict(self.X)
 
-dataset_name = st.sidebar.selectbox('Choose your desired dataset please: ',
-                                    ('Aggregation', 'Blobs', 'Circles', 'Moons'))
+    def __dispaly(self):
+        plt.style.use('dark_background')
+        # Plot original data
+        plt.figure(figsize=(8, 6))
+        plt.scatter(self.X[:, 0], self.X[:, 1], c='blue', alpha=0.7)
+        plt.title("Original Data")
+        plt.xlabel("Feature 1")
+        plt.ylabel("Feature 2")
+        st.pyplot(plt)
 
-clustering_algorithm = st.sidebar.selectbox('Choose Clustering Algorithm: ',
-                                            ('K-Means', 'Agglomerative', 'DBSCAN'))
-st.write(f'Selected dataset: {dataset_name}')
+        # Plot clustering results
+        plt.figure(figsize=(8, 6))
+        plt.scatter(self.X[:, 0], self.X[:, 1], c=self.cluster_labels, cmap='plasma', alpha=0.7)
+        plt.title("Clustering Algorithm: Class Assignments")
+        plt.xlabel("Feature 1")
+        plt.ylabel("Feature 2")
+        st.pyplot(plt)
 
 
-
-X, y = load_dataset(dataset_name=dataset_name)
-# st.write(
-#     f"""
-#     Dataset info:\n
-#     Shape = {X.shape}\n
-#     No. of clusters: {len(unique(y))}
-#     """
-# )
-
-# Apply clustering algorithm
-clusterer = load_algorithm(clustering_algorithm=clustering_algorithm)
-cluster_labels = clusterer.fit_predict(X)
-
-# Display clustering results
-# st.write("Clustering Results:")
-# st.write(cluster_labels)
-
-# Plot original data
-plt.figure(figsize=(8, 6))
-plt.scatter(X[:, 0], X[:, 1], c='blue', alpha=0.7)
-plt.title("Original Data")
-plt.xlabel("Feature 1")
-plt.ylabel("Feature 2")
-st.pyplot(plt)
-
-# Plot clustering results
-plt.figure(figsize=(8, 6))
-plt.scatter(X[:, 0], X[:, 1], c=cluster_labels, cmap='viridis', alpha=0.7)
-plt.title("Clustering Algorithm: Class Assignments")
-plt.xlabel("Feature 1")
-plt.ylabel("Feature 2")
-st.pyplot(plt)
+if __name__ == '__main__':
+    app = StreamlitApp()
+    app.run()
