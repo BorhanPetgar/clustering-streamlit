d761920fb073374b8ba5dec876200b0d44ed561e
BorhanPetgar <borhanpetgar40@gmail.com>
2024-05-03 13:21:59 +0330

    title is done

---
+++ clf_app.py
@@ -0,0 +1,10 @@
+import streamlit as st
+from sklearn import datasets
+
+
+st.title(':blue[C]:red[l]:blue[a]:red[s]:blue[s]:red[i]:blue[f]:red[i]:blue[c]:red[a]:blue[t]:red[i]:blue[o]:red[n] :sunglasses:')
+
+st.write("""
+         # Explore **classification** algorithms on different dataset
+         """)
+
